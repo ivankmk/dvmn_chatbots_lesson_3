f1678c424f62f722ea240507a1cca62aefb4d65f
IvanKMK <kumeyko@hotmail.com>
2022-07-18 21:07:58 +0200

    re-wrote init

--- log_handler.py
+++ log_handler.py
@@ -1,14 +1,14 @@
 import logging
 import telegram
-import os
 
 
 class MyLogsHandler(logging.Handler):
- 
+
+    def __init__(self, tg_token, tg_chat_id):
+        self.bot = telegram.Bot(token=tg_token)
+        self.chat_id = tg_chat_id
+
     def emit(self, record):
-        tg_token = os.environ.get('TG_TOKEN')
-        tg_chat_id = os.environ.get('TG_CHAT_ID')
-        bot_logger = telegram.Bot(token=tg_token)
         log_entry = self.format(record)
-        bot_logger.send_message(
-            chat_id=tg_chat_id, text=log_entry)
+        self.bot.send_message(
+            chat_id=self.tg_chat_id, text=log_entry)
